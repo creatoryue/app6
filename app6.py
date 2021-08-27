31d0a742bbceb035c21d21cf909d9a49bbbeed08
GENG YUE <86158319+creatoryue@users.noreply.github.com>
2021-08-27 16:42:47 +0800

    Update app6.py

--- app6.py
+++ app6.py
@@ -1,27 +1,3 @@
-import logging
-from pathlib import Path
-import streamlit as st
-import pydub
-import numpy as np
-import queue
-import matplotlib.pyplot as plt
+
 import librosa
-#import librosa.display
-
-from src import loadModel
-import time
-
-# from aiortc.contrib.media import MediaRecorder
-
-from streamlit_webrtc import (
-    # AudioProcessorBase,
-    ClientSettings,
-    # VideoProcessorBase,
-    WebRtcMode,
-    webrtc_streamer,
-)
-
-cnn = loadModel.CNN
-cnn.model = cnn.loadTrainingModel(self=cnn)
-classes = ['COPD-Mild', 'COPD-Severe', 'Interstitial Lung Disease', 'Normal']
 
