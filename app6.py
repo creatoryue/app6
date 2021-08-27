89f0c4f89eafedf7635e1cce6fcc8bf649ab6b29
GENG YUE <86158319+creatoryue@users.noreply.github.com>
2021-08-27 15:25:57 +0800

    Update app6.py

--- app6.py
+++ app6.py
@@ -21,25 +21,3 @@ from streamlit_webrtc import (
     webrtc_streamer,
 )
 
-
-HERE = Path(__file__).parent
-logger = logging.getLogger(__name__)
-
-WEBRTC_CLIENT_SETTINGS = ClientSettings(
-    rtc_configuration={"iceServers": [{"urls": ["stun:stun.l.google.com:19302"]}]},
-    media_stream_constraints={
-        "video": False,
-        "audio": True,
-    },
-)
-
-import os
-ROOT_DIR = os.path.dirname(os.path.abspath(__file__))
-DATA_DIR = os.path.join(ROOT_DIR, 'data')
-
-
-# Load Model 
-cnn = loadModel.CNN
-cnn.model = cnn.loadTrainingModel(cnn)
-classes = ['COPD-Mild', 'COPD-Severe', 'Interstitial Lung Disease', 'Normal']
-
