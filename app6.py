fd90e23cba8008ac4beb53bc8a191143959ae3cd
GENG YUE <86158319+creatoryue@users.noreply.github.com>
2021-08-27 17:27:40 +0800

    Update app6.py

--- app6.py
+++ app6.py
@@ -112,3 +112,23 @@ def main():
             s2 = np.round(float(data_pred[0,data_pred_class])*100, 4) # s1 is the percentage of the predicted class
             st.text("Predict class: {} for {}%".format(s1, s2))
 
+if __name__ == '__main__':
+    import os
+
+    DEBUG = os.environ.get("DEBUG", "false").lower() not in ["false", "no", "0"]
+
+    logging.basicConfig(
+        format="[%(asctime)s] %(levelname)7s from %(name)s in %(pathname)s:%(lineno)d: "
+        "%(message)s",
+        force=True,
+    )
+
+    logger.setLevel(level=logging.DEBUG if DEBUG else logging.INFO)
+    
+    st_webrtc_logger = logging.getLogger("streamlit_webrtc")
+    st_webrtc_logger.setLevel(logging.DEBUG)
+
+    fsevents_logger = logging.getLogger("fsevents")
+    fsevents_logger.setLevel(logging.WARNING)
+    
+    main()
