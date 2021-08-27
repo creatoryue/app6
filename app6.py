57580c7326c61cee269b1a7a72027bf6cd2c19a5
GENG YUE <86158319+creatoryue@users.noreply.github.com>
2021-08-27 17:24:51 +0800

    Update app6.py

--- app6.py
+++ app6.py
@@ -1,6 +1,47 @@
+import logging
+from pathlib import Path
+import streamlit as st
+import pydub
+import numpy as np
+# import queue
+import matplotlib.pyplot as plt
 import librosa
-import tensorflow
+import librosa.display
+
+from src import loadModel
+import time
+
+# from aiortc.contrib.media import MediaRecorder
+
+from streamlit_webrtc import (
+    # AudioProcessorBase,
+    ClientSettings,
+    # VideoProcessorBase,
+    WebRtcMode,
+    webrtc_streamer,
+)
 
 
-"""Lung Classification Web APP"""
+HERE = Path(__file__).parent
+logger = logging.getLogger(__name__)
 
+WEBRTC_CLIENT_SETTINGS = ClientSettings(
+    rtc_configuration={"iceServers": [{"urls": ["stun:stun.l.google.com:19302"]}]},
+    media_stream_constraints={
+        "video": False,
+        "audio": True,
+    },
+)
+
+import os
+ROOT_DIR = os.path.dirname(os.path.abspath(__file__))
+DATA_DIR = os.path.join(ROOT_DIR, 'data')
+
+# def saveWavFile(fn):    
+#     WAVE_OUTPUT_FILE = os.path.join(DATA_DIR, "{}.wav".format(fn))
+#     return WAVE_OUTPUT_FILE
+
+# Load Model 
+cnn = loadModel.CNN
+cnn.model = cnn.loadTrainingModel(self=cnn)
+classes = ['COPD-Mild', 'COPD-Severe', 'Interstitial Lung Disease', 'Normal']
