d61411c818cd3e09b87bf06a1d164c5398aeb3e2
GENG YUE <86158319+creatoryue@users.noreply.github.com>
2021-08-29 00:49:12 +0800

    Update app6.py

--- app6.py
+++ app6.py
@@ -81,7 +81,7 @@ def main():
     state_button_test1 = st.button('Test1')
     if state_button_test1:
         data = librosa.load(DATA_DIR_VOICE_1)
-
+        data_pred = cnn.samplePred(cnn, data)
             
             
     if webrtc_ctx.audio_receiver:
